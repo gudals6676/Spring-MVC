9da2eef734225f982db754891ef743bb87f0ca6a
박형민 <82852370+gudals6676@users.noreply.github.com>
2021-07-26 14:27:32 +0900

    Spring/MVC Study

---
+++ SpringMVC/src/main/java/kr/smhrd/mapper/BoardMapper.java
@@ -0,0 +1,34 @@
+package kr.smhrd.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import kr.smhrd.domain.BoardVO;
+
+@Mapper // -> 일반적으로 생략!
+public interface BoardMapper {
+  // SQL --> X Mapper File(XML)
+   public List<BoardVO> boardList();
+   
+   @Select("select * from tbl_board order by idx desc") //idx를 기준으로 내림차순
+   public List<BoardVO> boardListAjax();
+   
+   public void boardInsert(BoardVO vo); // insert SQL~
+   
+   @Select("select * from tbl_board where idx=#{idx}") // xml에 쓸필요 없이 여기서 바로 가능!!!
+   public BoardVO boardContent(int idx);// select SQL~
+   
+   public void boardDelete(int idx);
+   
+   @Update("update tbl_board set title = #{title}, contents = #{contents} where idx = #{idx}")
+   public void boardUpdate(BoardVO vo);
+   
+   @Delete("delete from tbl_board where idx = #{idx}")
+   public int boardDeleteAjax(int idx);
+   
+   
+ }
